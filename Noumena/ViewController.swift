89ce0d8669c587f06a56ad1dc6c8bf9133d5cc3b
PD1ce <padeisinger@gmail.com>
2015-09-24 19:58:32 -0500

    First commit

---
+++ Noumena/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Noumena
+//
+//  Created by Philip Deisinger on 9/24/15.
+//  Copyright (c) 2015 PDice. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
